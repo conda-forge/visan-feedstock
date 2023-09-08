121c8e65639432d929472a36b18bd889744af2c9
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-09-08 12:44:36 +0200

    Add CMAKE_CROSSCOMPILING_EMULATOR wrapper script for VTK build tools

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,26 @@
 mkdir build
 cd build
 
+# This wrapper script is needed for the VTK build tools when cross compiling.
+# When e.g. vtkWrapPython is invoked from the target conda environment in
+# $PREFIX, CMake will pass the command invocation to this wrapper script.
+# We then redirect the call to the executable in the build conda environment
+# in $BUILD_PREFIX (which will be of the proper host architecture).
+cat << _EOF > crosswrapper.sh
+#!/bin/bash
+executable=\$1
+shift
+if [[ \$executable == "\$PREFIX"* ]] ; then
+  executable=\$BUILD_PREFIX\${executable#\$PREFIX}
+fi
+\$executable \$@
+_EOF
+chmod 755 crosswrapper.sh
+
 export CMAKE_LIBRARY_PATH=$PREFIX/lib
 
 cmake $CMAKE_ARGS \
+  -DCMAKE_CROSSCOMPILING_EMULATOR="$PWD/crosswrapper.sh" \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
